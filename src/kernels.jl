aa5b6bcd82592281164341eb5e964bfc1509044e
Thomas Marks <marksta@umich.edu>
2020-09-01 17:29:14 -0400

    Add M4, M5 and M6 kernels and test

--- src/kernels.jl
+++ src/kernels.jl
@@ -4,15 +4,25 @@ const σ4 = (2/3, 10/7/π, 1/π)
 const σ5 = (1/24, 96/1199/π, 1/20/π)
 const σ6 = (1/120, 7/478/π, 1/120/π)
 
+const M4 = σ4 .* @piecewise_polynomial begin
+    0.0 => p"0.0"
+    1.0 => p"0.25*(2-q)^3 - (1 - q)^3"
+    2.0 => p"0.25*(2-q)^3"
+    _   => p"0.0"
+end
 
-const test_p = p"2x"
+const M5 = σ5 .* @piecewise_polynomial begin
+    0.0 => p"0.0"
+    0.5 => p"(2.5 - q)^4 - 5*(1.5 - q)^4 + 10*(0.5 - q)^4"
+    1.5 => p"(2.5 - q)^4 - 5*(1.5 - q)^4"
+    2.5 => p"(2.5 - q)^4"
+    _   => p"0.0"
+end
 
-const _M6 = @piecewise_polynomial begin
+const M6 = σ6 .* @piecewise_polynomial begin
     0.0 => p"0.0"
     1.0 => p"(3 - q)^5 - 6*(2 - q)^5 + 15*(1 - q)^5"
     2.0 => p"(3 - q)^5 - 6*(2 - q)^5"
     3.0 => p"(3 - q)^5"
-    _ => p"0.0"
+    _   => p"0.0"
 end
-
-#const M6 = _M6 .* σ6
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,24 @@ using Test
 
 #default(;xlims=(0,3), ylims=(-2, 1), label="")
 
+function M6_hardcoded(q)
+    if q < 0.0
+        return 0.0
+    elseif q < 1.0
+        return (3 - q)^5 - 6*(2 - q)^5 + 15*(1 - q)^5
+    elseif q < 2.0
+        return (3 - q)^5 - 6*(2 - q)^5
+    elseif q < 3.0
+        return (3 - q)^5
+    else
+        return 0.0
+    end
+end
+
 @testset "SPH.jl" begin
-    
+   xs = rand(1000) * 6 .- 1
+   fs_hardcoded = M6_hardcoded.(xs)
+   fs_procedural = SPH._M6.(xs)
+   @test isapprox.(fs_hardcoded, fs_procedural, atol=1e-8) |> all
+   #@show xs[a], fs_hardcoded[a], fs_procedural[a]
 end
