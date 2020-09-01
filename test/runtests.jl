3779303b1e547c7ad12f40d9cdd598046a34689f
Thomas Marks <marksta@umich.edu>
2020-09-01 18:10:30 -0400

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,22 +5,23 @@ using Test
 
 function M6_hardcoded(q)
     if q < 0.0
-        return 0.0
+        res = 0.0
     elseif q < 1.0
-        return (3 - q)^5 - 6*(2 - q)^5 + 15*(1 - q)^5
+        res = (3 - q)^5 - 6*(2 - q)^5 + 15*(1 - q)^5
     elseif q < 2.0
-        return (3 - q)^5 - 6*(2 - q)^5
+        res = (3 - q)^5 - 6*(2 - q)^5
     elseif q < 3.0
-        return (3 - q)^5
+        res = (3 - q)^5
     else
-        return 0.0
+        res = 0.0
     end
+    return res
 end
 
 @testset "SPH.jl" begin
    xs = rand(1000) * 6 .- 1
-   fs_hardcoded = M6_hardcoded.(xs)
-   fs_procedural = M6.(xs)
+   fs_hardcoded = M6_hardcoded.(xs) .* SPH.σ6[1]
+   fs_procedural = M6[1].(xs)
    @test isapprox.(fs_hardcoded, fs_procedural, atol=1e-8) |> all
    #@show xs[a], fs_hardcoded[a], fs_procedural[a]
 end
