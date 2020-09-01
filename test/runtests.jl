779d94c670e8dae4b27bc9f8c24cc76de32d2a5f
Thomas Marks <marksta@umich.edu>
2020-09-01 13:28:06 -0400

    Update project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,5 @@ using Test, Plots
 default(;xlims=(0,3), ylims=(-2, 1), label="")
 
 @testset "SPH.jl" begin
-    #@show SPH.Piecewise.p"2x"
-    p = plot(M6[1])
-    plot!(differentiate(M6[1]))
-    plot!(differentiate(M6[1], 2))
-    display(p)
     
 end
